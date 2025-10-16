e3c9145cff4dae8104e4107d64105bc7b73c9a4e
Matheus7857 <121524135+Matheus7857@users.noreply.github.com>
2025-10-16 19:30:53 -0400

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,9 @@
+// lib/db.ts
 import { Pool } from 'pg'
 
+// Cria um pool de conexões com o banco
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL, // Pega do arquivo .env.local
 })
 
 export default pool
